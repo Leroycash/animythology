32de01d95514c5a70642bff69fa74e426758f61c
leroy <leroycashmcgowaniii@gmail.com>
2023-03-01 20:55:51 -0600

    Initial Commit

---
+++ animythology/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  animythology
+//
+//  Created by Leroy McGowan on 2/5/23.
+//
+// Button(GroupBox(label: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Label")/*@END_MENU_TOKEN@*/) {
+   //  /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+// }) {
+ //    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+// }
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ animythology/animythologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  animythologyApp.swift
+//  animythology
+//
+//  Created by Leroy McGowan on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct animythologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
